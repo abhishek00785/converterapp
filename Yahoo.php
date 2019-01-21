58c6f7d309e76b863bed8f674a6bb1c58d5e7bef
abhishek00785 <46847849+abhishek00785@users.noreply.github.com>
2019-01-21 22:48:20 +0530

    Add files via upload

---
+++ Yahoo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Abhishek Mishra\CurrencyConverter\Converters;
+
+use Abhishek Mishra\CurrencyConverter\Services\HttpClient;
+
+class Yahoo extends Converter
+{
+   
+    private $base_url = 'http://query.yahooapis.com/v1/public/yql';
+    
+   
+    public function convert($from, $to, $amount)
+    {
+        $url_query       = 'select * from yahoo.finance.xchange where pair in ("' . $from . $to . '")';
+        $url             = $this->base_url . "?" . http_build_query([
+                'q'      => $url_query,
+                'format' => 'json',
+                'env'    => 'store://datatables.org/alltableswithkeys',
+            ]);
+        $client          = new HttpClient($url);
+        $request_result  = $client->run();
+        $value           = json_decode($request_result, true);
+        $rate            = $value['query']['results']['rate']['Rate'];
+        $currency_output = (float)$amount * $rate;
+        
+        return $currency_output;
+    }
+    
+}
---
+++ index.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+<?php
+
+// Composer autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Abhishek Mishra\CurrencyConverter\ConverterService;
+use Abhishek Mishra\CurrencyConverter\ConverterManager;
+
+array_shift($argv); // Get rid of the first value as it's the command invocation name.
+
+$command = array_shift($argv);
+
+switch ($command) {
+    
+    case 'convert':
+        $from      = array_shift($argv) ?: "USD";
+        $to        = array_shift($argv) ?: "EGP";
+        $amount    = array_shift($argv) ?: 1000;
+        $provider  = array_shift($argv) ?: 'google';
+        $converter = new ConverterService($provider);
+        echo "<pre>" . $converter->convert($from, $to, $amount) . "</pre>";
+        break;
+    
+    case 'list':
+        echo "The available currency conversion provider are: " . join(', ',
+                array_keys(ConverterManager::$drivers)) . ".\n";
+        break;
+    
+    default:
+        printHelp();
+        break;
+}
